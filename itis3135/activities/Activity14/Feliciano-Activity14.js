73476aac2229d4eaf00a95c81e4298e8788450d3
cfelici2 <89818265+cfelici2@users.noreply.github.com>
2021-11-17 21:33:14 -0500

    activity-14-initial-commit

--- itis3135/activities/Activity14/Feliciano-Activity14.js
+++ itis3135/activities/Activity14/Feliciano-Activity14.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
-	
+    $("#sorkin").click(function() {
+        $.ajax({
+            type: "get",
+            url: "json_files/sorkin.json",
+            //beforeSend: function() {},
+            timeout: 10000,
+            error: function(xhr, status, error) {
+                alert("Error: " + xhr.status + " - " + error);
+            },
+            dataType: "json",
+            success: function(data) {
+                var html = "";
+                $.each(data.items, function(key, value) {
+                    html += "<h1>" + value.title + "</h1>" 
+                    html += "<h2>" + value.month + "</h2>" 
+                    html += "<h3>" + value.speaker + "</h3>"
+                    html += "<img src=" + value.image.m + ">" 
+                    html += "<p>" + value.text + "</p>" 
+                });
+                $("main").html(html);
+            }
+        });
+    });
 }); // end ready
