81bdea965ba0231a57a06e895a7b61d6b2c08aab
cfelici2 <89818265+cfelici2@users.noreply.github.com>
2021-12-09 16:40:46 -0500

    Update Feliciano-Activity14.js

--- itis3135/activities/Activity14/Feliciano-Activity14.js
+++ itis3135/activities/Activity14/Feliciano-Activity14.js
@@ -3,22 +3,25 @@ $(document).ready(function() {
         $.ajax({
             type: "get",
             url: "json_files/" + this.title + ".json",
-            //beforeSend: function() {},
+            //beforeSend: function() {  },
             timeout: 10000,
             error: function(xhr, status, error) {
                 alert("Error: " + xhr.status + " - " + error);
             },
             dataType: "json",
             success: function(data) {
-                var html = "";
-                $.each(data.items, function(i, value) {
-                    html += "<h1>" + value.title + "</h1>" 
-                    html += "<h2>" + value.month + "</h2>" 
-                    html += "<h3>" + value.speaker + "</h3>"
-                    html += "<img src=" + value.image + ">" 
-                    html += "<p>" + value.text + "</p>" 
+                $("main").html("");
+                $.each(data, function() {
+                    $.each(this, function(key, value) {
+                        $("main").append(
+                            "<h1>" + value.title + "</h1>" +
+                            "<h2>" + value.month + "</h2>" +
+                            "<h3>" + value.speaker + "</h3>" +
+                            "<img src=" + value.image + ">" +
+                            "<p>" + value.text + "</p>" 
+                        );
+                    });
                 });
-                $("main").html(html);
             }
         });
     });
